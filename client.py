434b329d6b1b32e3b9ddb10bcc18e82ebd7e6e7e
a16su <33782391+a16su@users.noreply.github.com>
2022-11-24 09:30:43 +0000

    add unpack

--- client.py
+++ client.py
@@ -3,11 +3,10 @@ import typing
 
 from websocket import WebSocket
 
-from utils import Package, WsOp, get_danmu_info
+from utils import PackageHandler, WsOp, get_danmu_info
 
 
 class BiliBiliLiveDanmu:
-
     def __init__(self, room_id: int, uid: typing.Optional[int] = None):
         self._room_id = room_id
         self._uid = uid or 0
@@ -18,6 +17,12 @@ class BiliBiliLiveDanmu:
         self.client.connect(self._uri)
         self._on_message_callbacks = []
 
+    def send_str(self, data: str, op: WsOp):
+        self.client.send(PackageHandler.pack(data.encode("utf-8"), op=op))
+
+    def send_json(self, body: typing.Dict, op: WsOp):
+        self.send_str(json.dumps(body), op=op)
+
     def _say_hello(self):
         body = {
             "uid": self._uid,
@@ -25,13 +30,15 @@ class BiliBiliLiveDanmu:
             "protover": 3,
             "platform": "web",
             "type": 2,
-            "key": self._danmu_info.data.token
+            "key": self._danmu_info.data.token,
         }
-        self.client.send(Package.pack(json.dumps(body).encode(), WsOp.WS_OP_USER_AUTHENTICATION))
-        resp = self.client.recv()
-        print(resp)
-        self.client.close()
+        try:
+            self.send_json(body, WsOp.WS_OP_USER_AUTHENTICATION)
+            for i in range(3):
+                resp = self.client.recv()
+                print(PackageHandler.unpack(resp))
+        finally:
+            self.client.close()
 
     def add_on_message_callback(self, func: typing.Callable[[bytes], bytes]):
         self._on_message_callbacks.append(func)
-
--- utils.py
+++ utils.py
@@ -1,30 +1,57 @@
+import json
 import struct
-from typing import Optional, Tuple
+from typing import NamedTuple
 
+import brotli
 import httpx
 
-from _types import WsOp, DanMuInfo
+from _types import DanMuInfo, WsOp
 
 
-class Header:
+class Header(NamedTuple):
+    msg_len: int
+    header_len: int = 16
+    ver: int = 1
+    op: int = 1
+    seq: int = 1
+
+
+class WsMessageHandler:
+    @classmethod
+    def pack(cls):
+        raise NotImplementedError
+
+    @classmethod
+    def unpack(cls):
+        raise NotImplementedError
+
+
+class HeaderPkgHandler(WsMessageHandler):
     """
     msg_len header_len ver  op    seq
     \_____/ \_______/  \/   \/    \/
      4byte   2byte    2byte 4byte 4byte
 
     """
-    header_len: int = 16
-    ver: int = 1
-    op: int = 1
-    seq: int = 1
 
     @classmethod
-    def pack(cls, msg_len, header_len: Optional[int] = None, ver: Optional[int] = None, op: Optional[WsOp] = None,
-             seq: Optional[int] = None):
-        header_len = header_len or cls.header_len
-        ver = ver or cls.ver
-        op = op.value if op else cls.op
-        seq = seq or cls.seq
+    def pack(
+        cls,
+        msg_len: int,
+        header_len: int = WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH.value,
+        ver: int = WsOp.WS_HEADER_DEFAULT_VERSION.value,
+        op: int = WsOp.WS_HEADER_DEFAULT_OPERATION.value,
+        seq: int = WsOp.WS_HEADER_DEFAULT_SEQUENCE.value,
+    ):
+        """
+        pack head
+        Args:
+            msg_len: data_len + head_lean
+            header_len: 16
+            ver: 1
+            op: op code
+            seq: 1
+        """
         # struct.pack_into("!I", cls.buffer, 0, msg_len)
         # struct.pack_into("!H", cls.buffer, 4, header_len)
         # struct.pack_into("!H", cls.buffer, 6, ver)
@@ -33,30 +60,58 @@ class Header:
         return struct.pack("!I2H2I", msg_len, header_len, ver, op, seq)
 
     @classmethod
-    def unpack(cls, header_bytes: bytes) -> Tuple[int, WsOp]:
-        assert len(header_bytes) == cls.header_len
-        mes_len, header_len, ver, op, seq = struct.unpack("!I2H2I", header_bytes)
-        assert header_len == cls.header_len
-        return mes_len - header_len, WsOp(op)
+    def unpack(cls, header_bytes: bytes) -> Header:
+        assert len(header_bytes) == WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH.value
+        data_len, header_len, ver, op, seq = struct.unpack("!I2H2I", header_bytes)
+        assert header_len == WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH.value
+        return Header(data_len, header_len, ver, op, seq)
 
 
-class Package:
+class PackageHandler(WsMessageHandler):
     @classmethod
     def pack(cls, msg: bytes, op: WsOp):
         msg_len = len(msg)
-        total_len = msg_len + WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH
-        return Header.pack(total_len, op=op) + msg
+        total_len = msg_len + WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH.value
+        return HeaderPkgHandler.pack(total_len, op=op.value) + msg
 
     @classmethod
     def unpack(cls, package: bytes):
-        msg_len, op = Header.unpack(package[:Header.header_len])
+        header = HeaderPkgHandler.unpack(package[: WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH.value])
+        if header.msg_len < len(package):
+            cls.unpack(package=package[: header.msg_len])
+            op = WsOp(header.op)
+            if op != WsOp.WS_OP_MESSAGE and op != WsOp.WS_OP_CONNECT_SUCCESS:
+                if op == WsOp.WS_OP_HEARTBEAT_REPLY:
+                    body = {"count": struct.unpack("!I", package[WsOp.WS_PACKAGE_HEADER_TOTAL_LENGTH :])}
+        else:
+            index = WsOp.WS_PACKAGE_OFFSET.value
+            s = header.msg_len
+            a = ""
+            u = ""
+            body = []
+            breakpoint()
+            while index < len(package):
+                s = struct.unpack_from("!I", package, index)[0]
+                a = struct.unpack_from("!H", package, index + WsOp.WS_HEADER_OFFSET.value)[0]
+                try:
+                    if header.ver == WsOp.WS_BODY_PROTOCOL_VERSION_NORMAL.value:
+                        c = package[index + a : index + s].decode("utf-8")
+                        u = json.loads(c) if c else None
+                    elif header.ver == WsOp.WS_BODY_PROTOCOL_VERSION_BROTLI:
+                        l = package[index + a : index + s]
+                        h = brotli.decompress(l.decode("utf-8"))
+                        u = cls.unpack(h)
+                    u and body.append(u)
+                except Exception:
+                    print("error")
+                finally:
+                    index += s
+        return body
 
 
 def get_danmu_info(room_id: int) -> DanMuInfo:
     url = "https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo"
     with httpx.Client() as client:
-        resp = client.get(url, params={
-            "id": room_id
-        })
+        resp = client.get(url, params={"id": room_id})
         resp.raise_for_status()
         return DanMuInfo(**resp.json())
